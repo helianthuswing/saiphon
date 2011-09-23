8e5d471f898075e5715f67133a3037bd25eba687
Taiyo Kojima <kojima@fujisan.co.jp>
2011-09-23 16:32:22 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saiphon::Application.config.secret_token = '98d8388687f1685f64b7a7b73739cb6cfea3a03c276851c6791f49b224641f96227152d5f89622de6e858a6673b496400d5f0e5eb5b05ebaeb47815b2438689d'
